1b1f6e6ce861730bc814748349cd925b9f801887
Ralf Beckers <ralf.beckers@hotmail.com>
2025-12-16 17:32:57 +0100

    Update

--- Src/NistWebApiShare/Service/NistService.cs
+++ Src/NistWebApiShare/Service/NistService.cs
@@ -11,6 +11,19 @@ internal class NistService(Uri host, IAuthenticator? authenticator, string appNa
 
     //protected override string? AuthenticationTestUrl => ""; 
 
+
+    /// <summary>
+    /// Configures the provided <see cref="HttpClient"/> instance with specific default headers required for API requests.
+    /// This includes setting the User-Agent, Accept, and API version headers.
+    /// </summary>
+    /// <param name="client">The <see cref="HttpClient"/> to configure for GitHub API usage.</param>
+    /// <param name="appName">The name of the application, used as the User-Agent header value.</param>
+    protected override void InitializeClient(HttpClient client, string appName)
+    {
+        client.DefaultRequestHeaders.Add("User-Agent", appName);
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+    }
+
     protected override async Task ErrorHandlingAsync(HttpResponseMessage response, string memberName, CancellationToken cancellationToken)
     {
         if (!response.IsSuccessStatusCode)
@@ -24,6 +37,8 @@ internal class NistService(Uri host, IAuthenticator? authenticator, string appNa
         }
         await Task.Delay(0, cancellationToken);
     }
+
+
 
     #region Products
 
